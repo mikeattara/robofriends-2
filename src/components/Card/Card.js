f3f0abf6759a8334672a3944735fca6095960808
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-15 23:22:08 +0000

    restructure app

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => (
+  <div>
+    <img src="" alt="" />
+    <div>
+      <h2>Jane Doe</h2>
+      <p>jane@email.com</p>
+    </div>
+  </div>
+);
+
+export default Card;
