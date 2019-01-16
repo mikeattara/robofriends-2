69862ad2067607ec8f6fdac5ef29ba96b4c87d6e
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-16 03:06:23 +0000

    refactor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "tachyons";
-import CardList from "./components/CardList/CardList";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { robots } from "./data/robots";
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
