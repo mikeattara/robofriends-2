8b3e9d806faffb08ba58e03f3a39448fb357442d
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-16 21:47:02 +0000

    update styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "tachyons";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
