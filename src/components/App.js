c8a0e24fd0d182021b0ca1d2dff42b010c9b1f68
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-16 03:49:49 +0000

    create more components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Header from "./Header/Header";
 import CardList from "./CardList/CardList";
+import Footer from "./Footer/Footer";
 import { robots } from "../data/robots";
 
-export default () => <CardList robots={robots} />;
+export default () => (
+  <>
+    <Header />
+    <main>
+      <CardList robots={robots} />
+    </main>
+    <Footer />
+  </>
+);
