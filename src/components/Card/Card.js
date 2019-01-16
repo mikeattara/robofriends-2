f95c67aaa5a227de1dd046cda10a10819354b2ea
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-16 01:37:42 +0000

    dynamic components

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => (
-  <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-3">
-    <img src="https://robohash.org/test200x200?set=set2" alt="" />
+const Card = ({ id, name, email, username }) => (
+  <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-3 tc" id={id}>
+    <img src={`https://robohash.org/${id}200x200?set=set2`} alt="" />
     <div>
-      <h2>Jane Doe</h2>
-      <h3>jdoe</h3>
-      <p>jane.d@email.com</p>
+      <h2>{name}</h2>
+      <h3>{username.toLowerCase()}</h3>
+      <p>{email.toLowerCase()}</p>
     </div>
   </div>
 );
