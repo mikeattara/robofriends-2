155a306227162a9c0718df3b712845df6f878c1f
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-16 01:06:48 +0000

    add robots.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Card = () => (
-  <div>
-    <img src="" alt="" />
+  <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-3">
+    <img src="https://robohash.org/test200x200?set=set2" alt="" />
     <div>
       <h2>Jane Doe</h2>
-      <p>jane@email.com</p>
+      <h3>jdoe</h3>
+      <p>jane.d@email.com</p>
     </div>
   </div>
 );
