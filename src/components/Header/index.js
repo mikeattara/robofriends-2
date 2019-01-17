76b648407317dfc2c525b2c09987c7fbd8ba3a5b
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-17 22:12:19 +0000

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SearchBox from "../SearchBox/SearchBox";
+import SearchBox from "../SearchBox";
 
 const Header = ({ searchChange }) => (
   <header className="tc">
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Header from "../../components/Header/Header";
-import CardList from "../../components/CardList/CardList";
-import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header";
+import CardList from "../../components/CardList";
+import Footer from "../../components/Footer";
 import { robots } from "../../data/robots";
 import "./App.css";
 
