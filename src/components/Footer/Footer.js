3d7c4c2ba894dcfa2cbecb3a635f2f345036a78c
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-17 21:49:21 +0000

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer>
+  <footer className="tc">
     <p>
       Made with <span /> by {"💛"}
       <a href="https://github.com/mikeattara">Mike Perry Y Attara</a>
