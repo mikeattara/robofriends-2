dbefe25920318548138207d54fad08f6295bb419
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-02-01 14:29:42 -0500

    redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { createLogger } from "redux-logger";
+import thunkMiddleWare from "redux-thunk";
 import "tachyons";
 import App from "./containers/App";
 import { searchRobots } from "./reducers";
@@ -10,7 +11,10 @@ import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(
+  searchRobots,
+  applyMiddleware(thunkMiddleWare, logger)
+);
 
 const rootElement = document.getElementById("root");
 
