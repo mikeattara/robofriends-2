2322f333d728e5c2e5df6cf680a67c7df87e6135
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-02-01 14:22:52 -0500

    implement loger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
 import "tachyons";
 import App from "./containers/App";
 import { searchRobots } from "./reducers";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const rootElement = document.getElementById("root");
 
